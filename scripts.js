30b47b2c32113c78e2685ef84dd976f236f9015c
BrunoHolmes <52051840+BrunoHolmes@users.noreply.github.com>
2020-03-28 12:39:36 -0300

    Aula 3/5

--- scripts.js
+++ scripts.js
@@ -2,6 +2,16 @@ function onOff(){
     document
         .querySelector("#modal")
         .classList
-        .toggle("hide");   
+        .toggle("hide"); 
+        
+        document
+            .querySelector("body")
+            .classList
+            .toggle("hideScroll");
+
+            document
+                .querySelector("#modal")
+                .classList
+                .toggle("addScroll")
 }
 
